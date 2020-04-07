7688d615f143812c07c5238e460b1a2c119ca8a4
Le Floch Thomas <t.lefloch53@gmail.com>
2020-04-07 12:45:58 +0200

    test: fix undefined env variables

--- src/core/expected-env.ts
+++ src/core/expected-env.ts
@@ -4,4 +4,11 @@ export const getEnvDefaultValue = (entries: EnvVar[], varName: string) => {
   return entries.find(env => env.name === varName).default;
 };
 
-export const expectedEnvVariables: EnvVar[] = [{ name: 'PORT', default: '3000' }];
+export const expectedEnvVariables: EnvVar[] = [
+  { name: 'PORT', default: '3000' },
+  { name: 'PG_HOST' },
+  { name: 'PG_PORT', default: '5432' },
+  { name: 'PG_DATABASE', default: 'postgres' },
+  { name: 'PG_USER', default: 'postgres' },
+  { name: 'PG_PASSWORD' },
+];
